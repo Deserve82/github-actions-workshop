b9e757706e68fdd86524d663dc5bc499bdb0373d
deserve82 <inth8204@gmail.com>
2021-08-17 21:37:30 +0900

    member hello team actoin with parameter

--- .github/actions/hello-team/main.go
+++ .github/actions/hello-team/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
-	fmt.Println("Hello Kangho")
-	fmt.Println("Hello Justin")
+    // 깃헙 액션 변수명은 고정이 되어 있음 대문자로 꼭 써줘야함
+    leader := os.Getenv("INPUT_LEADER")
+	member := os.Getenv("INPUT_MEMBER")
+
+	fmt.Println("Hello " + leader)
+	fmt.Println("Hello " + member)
 }
