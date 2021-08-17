df2f183ebc88e8f63b5ebe1fa9733e6f980c46e9
deserve82 <inth8204@gmail.com>
2021-08-17 23:00:13 +0900

    test3

--- .github/actions/issue-maker/src/index.js
+++ .github/actions/issue-maker/src/index.js
@@ -1,23 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-async function run(){
-    const issueTitle = core.getInput("issueTitle");
-    const joke = core.getInput("joke");
-    const token = core.getInput("repoToken");
+async function run() {
+  const issueTitle = core.getInput("issueTitle");
+  const joke = core.getInput("joke");
+  const token = core.getInput("repoToken");
 
-    try{
-        const octokit = new github.Github(token);
-        const newIssue = await octokit.issues.create({
-            repo: github.context.repo.repo,
-            owner: github.context.repo.owner,
-            title: issueTitle,
-            // 이슈 마크다운 만들기 위해서
-            body: "![Joke Image](" + joke + ")"
-        });
-    } catch (error){
-        core.setFailed(error.message);
-    }
+  try {
+    const octokit = new github.GitHub(token);
+    const newIssue = await octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: "![Joke Image](" + joke + ")"
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
